72d258100c634f9e8c7d55cedbd1a19e0f41d9bb
Long Yonggang <longyg1986@163.com>
2018-02-27 00:16:55 +0800

    pm data load export

--- src/main/java/com/longyg/backend/TemplateRepository.java
+++ src/main/java/com/longyg/backend/TemplateRepository.java
@@ -4,6 +4,8 @@ import com.longyg.backend.ars.tpl.ExcelTemplate;
 import com.longyg.backend.ars.tpl.ExcelTemplateParser;
 import com.longyg.backend.ars.tpl.definition.objectmodel.ObjectModelTplDef;
 import com.longyg.backend.ars.tpl.definition.objectmodel.ObjectModelTplDefParser;
+import com.longyg.backend.ars.tpl.definition.pmdataload.PmDataLoadTplDef;
+import com.longyg.backend.ars.tpl.definition.pmdataload.PmDlTplDefParser;
 import com.longyg.backend.ars.tpl.definition.userstory.TemplateDefParser;
 import com.longyg.backend.ars.tpl.definition.userstory.TemplateDefinition;
 
@@ -11,9 +13,11 @@ public class TemplateRepository {
     private static final String XLS_TPL_PATH = "template.xls";
     private static final String US_TPL_DEF_PATH = "userstory.template.xml";
     private static final String OM_TPL_DEF_PATH = "objectmodel.template.xml";
+    private static final String PMDL_TPL_DEF_PATH = "pmdataload.template.xml";
 
     private static TemplateDefinition usTplDef;
     private static ObjectModelTplDef omTplDef;
+    private static PmDataLoadTplDef pmDlTplDef;
     private static ExcelTemplate template;
 
     private static boolean initialized = false;
@@ -28,12 +32,16 @@ public class TemplateRepository {
         ObjectModelTplDefParser objectModelTplDefParser = new ObjectModelTplDefParser();
         ObjectModelTplDef omTplDef = objectModelTplDefParser.parse(OM_TPL_DEF_PATH);
 
+        PmDlTplDefParser pmDlTplDefParser = new PmDlTplDefParser();
+        PmDataLoadTplDef pmDlTplDef = pmDlTplDefParser.parse(PMDL_TPL_DEF_PATH);
+
         ExcelTemplateParser xlsTplParser = new ExcelTemplateParser();
         ExcelTemplate template = xlsTplParser.parse(XLS_TPL_PATH, usTplDef);
 
         TemplateRepository.template = template;
         TemplateRepository.usTplDef = usTplDef;
         TemplateRepository.omTplDef = omTplDef;
+        TemplateRepository.pmDlTplDef = pmDlTplDef;
 
         initialized = true;
     }
@@ -61,4 +69,8 @@ public class TemplateRepository {
     public static void setOmTplDef(ObjectModelTplDef omTplDef) {
         TemplateRepository.omTplDef = omTplDef;
     }
+
+    public static PmDataLoadTplDef getPmDlTplDef() { return pmDlTplDef; }
+
+    public static void setPmDlTplDef(PmDataLoadTplDef pmDlTplDef) { TemplateRepository.pmDlTplDef = pmDlTplDef; }
 }
--- src/main/java/com/longyg/backend/ars/export/PmDataLoadExporter.java
+++ src/main/java/com/longyg/backend/ars/export/PmDataLoadExporter.java
@@ -1,14 +1,41 @@
 package com.longyg.backend.ars.export;
 
+import com.longyg.backend.TemplateRepository;
 import com.longyg.frontend.model.ars.ARS;
+import com.longyg.frontend.model.ars.pm.PmDataLoadSpec;
+import com.longyg.frontend.service.ArsService;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PmDataLoadExporter extends Exporter {
+    @Autowired
+    private ArsService arsService;
+
+    private HSSFRow titleTplRow;
+
+    private HSSFRow dataTplRow;
 
     public void export(ARS ars, HSSFWorkbook wb) {
         init(ars, wb);
+
+        exportPmDataLoad();
+    }
+
+    private void exportPmDataLoad() {
+        int sheetNo = TemplateRepository.getPmDlTplDef().getSheet();
+        HSSFSheet sheet = this.wb.getSheetAt(sheetNo);
+
+        PmDataLoadSpec spec = arsService.findPmDL(this.ars.getPmDataLoad());
+
+        int titleTplRowNo = TemplateRepository.getPmDlTplDef().getTitleRow();
+        this.titleTplRow = sheet.getRow(titleTplRowNo);
+
+        int dataTplRowNo = TemplateRepository.getPmDlTplDef().getDataRow();
+        this.dataTplRow = sheet.getRow(dataTplRowNo);
 
 
     }
